772806e206dd0c5a4192b4dbab824c7db002e611
Guy  Cizner <46866327+ciznerguy@users.noreply.github.com>
2025-10-05 18:55:20 +0300

    Add project files.

---
+++ Actor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNember1
+{
+    public class Actor
+    {
+        // תכונות פרטיות בלבד
+        private int id;
+        private int age;
+        private int numberOfShows;
+
+        public Actor(int id, int age)
+        {
+            this.id = id;
+            this.age = age;
+            this.numberOfShows =0;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }   
+        public int GetNumberOfShows()
+        {
+            return this.numberOfShows;
+        }
+
+        public void AddShow()
+        {
+            this.numberOfShows++;
+        }
+
+
+
+    }
+    
+}
